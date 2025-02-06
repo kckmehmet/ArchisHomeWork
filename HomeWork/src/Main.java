ea558b91acc14ece60596297b51257e2e344864a
mehmet <mehmetkucukk_@outlook.com>
2025-02-06 14:00:16 +0300

    not hesaplama faiz hesaplama ve çift tek yapıldı

--- HomeWork/src/Main.java
+++ HomeWork/src/Main.java
@@ -1,15 +1,79 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //ciftTek();
+        //faizHesaplama();
+        //notHesaplama();
+    }
+    private static void notHesaplama(){
+        System.out.println("\n"+"-----Puan Hesaplama-----"+"\n");
+        String[] dersler={"Mat","Fen","Türkçe","Tarih"};
+        double[][] notlar=new double[dersler.length][1];
+        Scanner scanner=new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Matematik Not Ortalamasını Giriniz: ");
+        double notMat= scanner.nextDouble();
+        System.out.print("\n"+"Fen Bilgisi Not Ortalamasını Giriniz: ");
+        double notFen= scanner.nextDouble();
+        System.out.print("\n"+"Türkçe Not Ortalamasını Giriniz: ");
+        double notTurkce=scanner.nextDouble();
+        System.out.print("\n"+"Tarih Not Ortalamasını Giriniz: ");
+        double notTarih= scanner.nextDouble();
+
+        double ortalama=(notFen+notMat+notTarih+notTurkce)/4;
+
+        if (ortalama>85){
+            System.out.println("Not Ortalamanız: "+ortalama);
+            System.out.println("Tebrikler Takdir Belgesi Kazandınız...");
+            notHesaplama();
+        } else if (ortalama>70) {
+            System.out.println("Not Ortalamanız: "+ortalama);
+            System.out.println("Tebrikler Teşşekür Belgesi Kazandınız...");
+            notHesaplama();
+        }else if (ortalama>=50){
+            System.out.println("Not Ortalamanız: "+ortalama);
+            System.out.println("Sınıfı Geçtiniz....");
+            notHesaplama();
+        }else {
+            System.out.println("Not Ortalamanız: "+ortalama);
+            System.out.println("Sınıfta Kaldınız...");
+            notHesaplama();
+        }
+
+    }
+    private static void faizHesaplama(){
+        System.out.println("-----Faiz Hesaplama-----"+"\n");
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Kredi Tutarını Giriniz: ");
+        int kredi=scanner.nextInt();
+        System.out.print("\n"+"Aylık Faiz Oranını Girniz: ");
+        double faiz= scanner.nextDouble();
+        System.out.print("\n"+"Vade Süresini giriniz: ");
+        int vade=scanner.nextInt();
+
+        double toplam=(kredi*faiz)*(vade);
+
+        System.out.println("Toplam Geri Ödeme Tutarı: "+toplam);
+    }
+    private static void ciftTek(){
+        System.out.println("-----Çift mi Tek mi-----"+"\n");
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Bir Sayı giriniz: ");
+        int num=scanner.nextInt();
+
+        if (num%2==0){
+            System.out.println("Bu Sayı Çift.."+"\n");
+            ciftTek();
+        }else {
+            System.out.println("Bu Sayı Tek.."+"\n");
+            ciftTek();
         }
     }
+
 }
